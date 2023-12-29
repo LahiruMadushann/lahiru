6ed4977930f272f197321ca6de0fa2839811c173
LahiruMadushann <lahirumadushandl@gmail.com>
2023-12-29 20:03:59 +0530

    Modified Path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <main className='bg-slate-300/20 h-full'>
-        <Router basename="/lahiru">
+        <Router basename="/">
             <Navbar />
             <Routes>
                 <Route path='/home' element={<Home />} />
